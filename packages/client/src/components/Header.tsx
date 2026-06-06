243f28fddfd3db989b36ad2d87655c6d9062abb5
dio067 <yousefnas06@gmail.com>
2026-06-06 22:26:54 +0400

    feat(client): update the header component

---
+++ packages/client/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className='w-full bg-[#111111] py-4 px-8 border-b border-[#2a2a2a] mb-4 '>
+      <h1 className='text-[#e8e8e8] font-bold text-2xl'>Docket</h1>
+    </div>
+  );
+}
