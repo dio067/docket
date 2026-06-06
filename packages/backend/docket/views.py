2a23c6ccaff3d1fdb55d563281c0c4fb06ce20cb
dio067 <yousefnas06@gmail.com>
2026-06-06 14:56:11 +0400

    feat(routes): add viewset to implement CRUD to docket

--- packages/backend/docket/views.py
+++ packages/backend/docket/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Docket
+from .serializers import DocketSerializer
 
 # Create your views here.
+class DocketView(model.ModelViewSet):
+    serializers_class = DocketSerializer
+    queryset = Docket.objects.all()
