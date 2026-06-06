54021041dc9c6f65ffb9085d9ef3833a64fa69a1
dio067 <yousefnas06@gmail.com>
2026-06-06 22:30:12 +0400

    feat(client): wrap all component inside the app and pass props

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import ItemList from "./components/ItemList";
+import AddItem from "./components/AddItem";
+import type item from "./types/item";
+import Header from "./components/Header";
+
 export default function App() {
-  return <div></div>;
+  const [items, setItems] = useState<item[]>([]);
+  const [newItem, setNewItem] = useState<item>({
+    id: "",
+    title: "",
+    description: "",
+    completed: false,
+  });
+  return (
+    <div className='bg-[#0f0f0f] w-full min-h-screen m-0 p-0'>
+      <Header />
+      <AddItem newItem={newItem} setNewItem={setNewItem} setItems={setItems} />
+      <ItemList items={items} setItems={setItems} />
+    </div>
+  );
 }
