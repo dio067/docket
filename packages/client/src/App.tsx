eafca5e49534147afbeeae5c58285193431e98b5
dio067 <yousefnas06@gmail.com>
2026-06-07 15:31:42 +0400

    refactor(client): move App UI to sperate page called Home and use Routes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,41 +1,14 @@
-import { useState, useEffect } from "react";
-import useDocket from "./hooks/use-docket";
-import ItemList from "./components/ItemList";
-import AddItem from "./components/AddItem";
-import type item from "./types/item";
-import Header from "./components/Header";
-import { getItems } from "./api/docket";
+import { Route, Routes, BrowserRouter } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 export default function App() {
-  const [items, setItems] = useState<item[]>([]);
-  const [newItem, setNewItem] = useState<item>({
-    id: "",
-    title: "",
-    description: "",
-    completed: false,
-  });
-
-  const { handleAdd, handleRemove } = useDocket(setItems, setNewItem);
-
-  useEffect(() => {
-    getItems()
-      .then((res) => {
-        setItems(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
-    <div className='bg-[#0f0f0f] w-full min-h-screen m-0 p-0'>
-      <Header />
-      <AddItem
-        newItem={newItem}
-        setNewItem={setNewItem}
-        setItems={setItems}
-        handleAdd={handleAdd}
-      />
-      <ItemList items={items} handleRemove={handleRemove} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
